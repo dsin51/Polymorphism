a487b1261a8ed776e38fd3d9035dc643b6baa7d8
dsin51 <dsinghal3@sapient.com>
2016-02-10 16:32:00 +0530

    Checked All switch cases

--- Polymorphism/src/com/training/testcases/ShowRoomDefaultCaseTest.java
+++ Polymorphism/src/com/training/testcases/ShowRoomDefaultCaseTest.java
@@ -1,8 +1,5 @@
 package com.training.testcases;
 
-import com.training.ifaces.*;
-import com.training.domains.*;
-
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
@@ -15,21 +12,25 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-import com.training.domains.ShowRoom;
+import com.training.domains.LuxuryCar;
+import com.training.domains.NewShowRoom;
+import com.training.domains.PassengerCar;
+import com.training.domains.SportsBike;
+import com.training.domains.SportsCar;
+import com.training.ifaces.Automobile;
+
 
 @RunWith(Parameterized.class)
-public class ShowRoomTest {
+public class ShowRoomDefaultCaseTest {
 
 	NewShowRoom newShowRoom = null;
 	private int key;
-	private Class cls;
 	
-	public ShowRoomTest(int key, Class cls) {
+	
+	public ShowRoomDefaultCaseTest(int key) {
 		super();
 		this.key = key;
-		this.cls = cls;
 	}
-	
 
 	@Before
 	public void setUp() throws Exception {
@@ -39,25 +40,18 @@ public class ShowRoomTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	
 	@Parameters
 	public static Collection<Object[]> data() {
-		Object[][] data = new Object[][]{{1,PassengerCar.class},{2,LuxuryCar.class},
-												{3,SportsCar.class},{4,SportsBike.class}};
+		Object[][] data = new Object[][]{{5},{6},{12}};
 		return Arrays.asList(data);
 	}
-	
+
 	@Test
-	public void testValidCases() {
-		//showRoom.getItem(key);
+	public void testDefaultCase() {
 		Automobile auto = newShowRoom.getItem(key);
-		
-//		assertTrue(auto instanceof Automobile);
-		
-		assertEquals(cls, auto.getClass());
+		assertNull(auto);
 		
 	}
-	
-	
 
 }
---
+++ Polymorphism/src/com/training/testcases/TestSuite.java
@@ -0,0 +1,20 @@
+package com.training.testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+
+
+@RunWith(Suite.class)
+@SuiteClasses(value={ShowRoomTest.class,ShowRoomDefaultCaseTest.class})
+public class TestSuite {
+
+
+}
